f204b4482485493d0d2dc86e8b643dbd8ae2f2c3
Félix Saint-Genis <saintgenisfelix@gmail.com>
2020-03-20 16:13:53 +0100

    add navigation component to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
+    <Navigation v-if="currentUser"></Navigation>
     <router-view />
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+import Navigation from "./views/Navigation.vue";
+export default {
+  components: { Navigation },
+  computed: {
+    ...mapState(["currentUser"])
+  }
+};
 </script>
